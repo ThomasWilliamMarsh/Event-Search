3339b1f8c0ac5f010d7445498bd54f9e63baa2a1
tommarshbbc <tom.marsh@bbc.co.uk>
2021-01-15 15:23:43 +0000

    Fixed visibility modifiers

--- core/src/main/java/info/tommarsh/eventsearch/core/data/attractions/AttractionsRepository.kt
+++ core/src/main/java/info/tommarsh/eventsearch/core/data/attractions/AttractionsRepository.kt
@@ -7,7 +7,7 @@ interface AttractionsRepository {
 
     fun getAttractionsForQuery(query: String) : PagingSource<Int, AttractionModel>
 
-    fun getAttractionsForCategory(segment: String): PagingSource<Int, AttractionModel>
+    fun getAttractionsForCategory(category: String): PagingSource<Int, AttractionModel>
 
     suspend fun getAttraction(id: String) : AttractionModel
 }
