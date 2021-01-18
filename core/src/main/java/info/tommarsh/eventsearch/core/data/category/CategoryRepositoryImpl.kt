0b479929cc13622bfd4554c0468cac20dcdcfe30
tommarshbbc <tom.marsh@bbc.co.uk>
2021-01-18 14:45:13 +0000

    Use correct category ids

--- core/src/main/java/info/tommarsh/eventsearch/core/data/category/CategoryRepositoryImpl.kt
+++ core/src/main/java/info/tommarsh/eventsearch/core/data/category/CategoryRepositoryImpl.kt
@@ -9,10 +9,10 @@ internal class CategoryRepositoryImpl @Inject constructor() : CategoryRepository
     override fun getCategories(): List<CategoryModel> {
         return listOf(
                 CategoryResponse(id = "Music", name = "Music"),
-                CategoryResponse(id = "Sport", name = "Sport"),
+                CategoryResponse(id = "Sports", name = "Sports"),
                 CategoryResponse(id = "Arts", name = "Arts"),
                 CategoryResponse(id = "Arts & Theatre", name = "Arts, Theatre & Comedy"),
-                CategoryResponse(id = "Family & Attractions", name = "Family & Attractions")
+                CategoryResponse(id = "Family", name = "Family & Attractions")
         ).toDomainModel()
     }
 }
--- core/src/main/java/info/tommarsh/eventsearch/core/di/DataModule.kt
+++ core/src/main/java/info/tommarsh/eventsearch/core/di/DataModule.kt
@@ -8,7 +8,6 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import info.tommarsh.eventsearch.core.data.AttractionDetailsUseCase
