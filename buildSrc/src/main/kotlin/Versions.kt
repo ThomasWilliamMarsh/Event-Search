1a83a3fd27539ab601d81408f3f9a168b829ad4b
Tom Marsh <Tom.Marsh@thetrainline.com>
2021-07-13 14:58:37 +0100

    Update compose to RC-01

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
 
-    const val compose = "1.0.0-beta09"
+    const val compose = "1.0.0-rc01"
     const val dagger = "2.37"
     const val buildTools = "31-rc2"
     const val compileSdk = 30
@@ -15,7 +15,7 @@ object Versions {
     const val coroutines = "1.5.0-RC"
     const val appCompat = "1.3.0"
     const val composeViewModel = "1.0.0-alpha07"
-    const val navigation = "2.4.0-alpha03"
+    const val navigation = "2.4.0-alpha04"
     const val lifecycle = "2.2.0"
     const val paging = "3.0.0"
     const val datastore = "1.0.0-beta01"
@@ -25,9 +25,9 @@ object Versions {
     const val jodaTime = "2.10.6"
     const val moshi = "1.9.3"
     const val retrofit = "2.9.0"
-    const val accompanist = "0.12.0"
+    const val accompanist = "0.13.0"
     const val fragment = "1.3.4"
-    const val activity = "1.3.0-beta02"
+    const val activity = "1.3.0-rc01"
     const val material = "1.3.0"
     const val room = "2.3.0"
     const val appStartup = "1.0.0"
